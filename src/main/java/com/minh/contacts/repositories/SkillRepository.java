099cbca8bef2536cdfec7a6f06d83b7b8a1678f5
Huynh Ngoc Minh <huynhngocminh2511@gmail.com>
2019-09-12 21:57:37 +0700

    add repositories

---
+++ src/main/java/com/minh/contacts/repositories/SkillRepository.java
@@ -0,0 +1,7 @@
+package com.minh.contacts.repositories;
+
+import com.minh.contacts.models.Skill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SkillRepository extends JpaRepository<Skill, Integer> {
+}
